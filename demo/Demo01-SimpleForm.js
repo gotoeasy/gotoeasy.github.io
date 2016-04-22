46a6da49cffdb65d09f14f61ae3e7e2c30b71ef3
zhangming <sunmoon2222@163.com>
2016-04-22 17:39:00 +0800

    示例1

--- demo/Demo01-SimpleForm.js
+++ demo/Demo01-SimpleForm.js
@@ -4,17 +4,14 @@ define(['gotoEasy'], function($easy) {
 	return function(){
 
 		// 定义数据
-		var data = {
-					formData:{},
-					jsonStr:''
-					};
+		var data = {};
 
 		// 绑定数据
 		$easy.bind(data);
 
 		// 数据变更时，显示数据的JSON字符串
 		$easy.on('datachange', function(){
-			data.set('jsonStr', JSON.stringify(data.formData, null, '  '));
+			document.getElementById('jsonStr').innerHTML = '<pre>' + JSON.stringify(data, null, '  ') + '</pre>'
 		});
 	};
 
