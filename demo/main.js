5d583f79dd9d65fac697ccfed1cb32ccde2af7b0
zhangming <sunmoon2222@163.com>
2016-04-22 15:34:33 +0800

    初稿

--- demo/main.js
+++ demo/main.js
@@ -8,7 +8,7 @@ require.config({
 });
 
 var url = location.href.split("?")[0];
-var page = url.substring(url.lastIndexOf('/') + 1).split(".")[0];
+var page = url.substring(url.lastIndexOf('/') + 1).split(".")[0] || 'index';
 
 require(['domReady', page], function(domReady, fn){
 	domReady(function() {
