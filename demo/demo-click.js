64314ca0c7c8191adebbbb191e6561245d99c350
zhangming <sunmoon2222@163.com>
2016-04-27 14:58:13 +0800

    data-bind="click:code~"的例子

--- demo/demo-click.js
+++ demo/demo-click.js
@@ -5,7 +5,6 @@ define(['gotoEasy'], function($easy) {
 
 		// 定义数据
 		var data = {
-			count: 5,
 			details: [{ id: 1, name: '电风扇', price: 200, count: 2 },
 					{ id: 2, name: '剃须刀', price: 198, count: 1 },
 					{ id: 3, name: '电视', price: 2000, count: 3 }, 
@@ -23,7 +22,6 @@ define(['gotoEasy'], function($easy) {
 			},
 			
 			fnSubmit: function() {
-					alert(JSON.stringify(this, null, '  '))
 				alert(JSON.stringify(data, null, '  '));
 			}
 		};
@@ -31,14 +29,6 @@ define(['gotoEasy'], function($easy) {
 		// 绑定数据
 		$easy.bind(data);
 
-		// ------------显示数据的JSON字符串--------------
-		// 初期显示数据的JSON字符串
-		document.getElementById('jsonStr').innerHTML = '<pre>' + JSON.stringify(data, null, '  ') + '</pre>'
-		// 数据变更时，刷新显示数据的JSON字符串
-		$easy.on('datachange', function(){
-			document.getElementById('jsonStr').innerHTML = '<pre>' + JSON.stringify(data, null, '  ') + '</pre>'
-		});
-		// ----------------------------------------------
 	};
 
 });
