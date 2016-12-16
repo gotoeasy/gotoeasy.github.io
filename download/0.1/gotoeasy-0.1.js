577c97d264557c41a397603cbe114909e98ab00d
zhangming <sunmoon2222@163.com>
2016-12-16 18:20:53 +0800

    new

--- download/0.1/gotoeasy-0.1.js
+++ download/0.1/gotoeasy-0.1.js
@@ -1,9 +1,9 @@
 /* gotoEasy 0.1, (c) 2016 Zhang Ming, @license MIT (http://gotoeasy.github.io) */
 !function(window, document) {
     function initOptionValues() {
-        DEBUG = null == Options.debug ? 1 : Options.debug, DOM_ATTR_BIND = Options.domAttrBind || (Options.domAttrBind = "data-bind"), 
-        SELECTOR_DATA_BIND = S_LEFT_ZKH + DOM_ATTR_BIND + S_RIGHT_ZKH, DATA_KEY_FN_ID = Options.dataKeyFnId || (Options.dataKeyFnId = "_$id"), 
-        DATA_KEY_FN_PARENT = Options.dataKeyFnParent || (Options.dataKeyFnParent = "_$parent"), 
+        DEBUG = null == Options.debug ? 1 : Options.debug, IS_REMOVE_ATTR_BIND = null == Options.removeAttrDataBind ? 1 : Options.removeAttrDataBind, 
+        DOM_ATTR_BIND = Options.domAttrBind || (Options.domAttrBind = "data-bind"), SELECTOR_DATA_BIND = S_LEFT_ZKH + DOM_ATTR_BIND + S_RIGHT_ZKH, 
+        DATA_KEY_FN_ID = Options.dataKeyFnId || (Options.dataKeyFnId = "_$id"), DATA_KEY_FN_PARENT = Options.dataKeyFnParent || (Options.dataKeyFnParent = "_$parent"), 
         DATA_KEY_FN_ROOT = Options.dataKeyFnRoot || (Options.dataKeyFnRoot = "_$root"), 
         DATA_KEY_FN_DATA = Options.dataKeyFnData || (Options.dataKeyFnData = "_$data"), 
         BIND_REF_ROOT = Options.bindRefRoot || (Options.bindRefRoot = "$root"), BIND_REF_PARENT = Options.bindRefParent || (Options.bindRefParent = "$parent"), 
@@ -20,7 +20,7 @@
         BIND_KEY_WITH = Options.bindKeyWith || (Options.bindKeyWith = "with"), BIND_KEY_IF = Options.bindKeyIf || (Options.bindKeyIf = "if"), 
         BIND_KEY_CLICK = Options.bindKeyClick || (Options.bindKeyClick = "click"), EVENT_DATA_CHAGE = Options.eventDataChage || (Options.eventDataChage = "datachange"), 
         EVENT_UPDATE_VIEW = Options.eventUpdateView || (Options.eventUpdateView = "updaueview"), 
-        UID_PREFIX_DATA = Options.uidPrefixData || (Options.uidPrefixData = "d"), UID_PREFIX_TEMPLATE = Options.uidPrefixTemplate || (Options.uidPrefixTemplate = "t");
+        UID_PREFIX_DATA = Options.uidPrefixData || (Options.uidPrefixData = "_D"), UID_PREFIX_TEMPLATE = Options.uidPrefixTemplate || (Options.uidPrefixTemplate = "_T");
     }
     function settings(opt) {
         return opt ? (extend(Options, opt), initOptionValues(), copyObject(Options)) : copyObject(Options);
@@ -107,11 +107,22 @@
     function set(data, key, value, forceUpdate) {
         if (!data[DATA_KEY_FN_ID]) return void (data[key] = value);
         var oldValue = data[key];
-        if (forceUpdate || isObject(value) || value !== oldValue) if (observe(value, data, key), 
-        data[key] = value, trigger(EVENT_DATA_CHAGE, data, key, value, oldValue), isArray(data)) {
+        if (forceUpdate || isObject(value) || value !== oldValue) if (isObject(oldValue) && (once(EVENT_CHECK_MEMORY, checkMapDataIdDataObject), 
+        notify(2e3, EVENT_CHECK_MEMORY)), observe(value, data, key), data[key] = value, 
+        trigger(EVENT_DATA_CHAGE, data, key, value, oldValue), isArray(data)) {
             var parent = data[DATA_KEY_FN_PARENT]();
             for (k in parent) parent[k] === data && notify(EVENT_UPDATE_VIEW, parent, k, value, oldValue);
         } else notify(EVENT_UPDATE_VIEW, data, key, value, oldValue);
+    }
+    function checkMapDataIdDataObject() {
+        var keys = [];
+        for (var k in _mapDataIdDataObject) keys.push(k);
+        for (var i = 0; i < keys.length; i++) checkMapDataId(keys[i]);
+    }
+    function checkMapDataId(dataid) {
+        async(function() {
+            document.querySelector("." + dataid) || delete _mapDataIdDataObject[dataid];
+        }, 100);
     }
     function getData(dataId) {
         return _mapDataIdDataObject[dataId];
@@ -150,7 +161,7 @@
     function getBindValue(data, bindText, fields, isEvent) {
         if (hasKey(data, bindText)) return fields && fields.push(getDataId(data) + "-" + bindText), 
         data[bindText];
-        var fnKey = getDataId(data) + "#" + bindText;
+        var fnKey = getDataId(data) + "&" + bindText;
         if (fields || !_fns[fnKey]) {
             var repls = [], body = bindText.replace(S_REG_QUOTE, function(match) {
                 var key = "{" + repls.length + "}";
@@ -171,14 +182,14 @@
         if (!fields) try {
             return _fns[fnKey](data);
         } catch (e) {
-            return void error("#2", _fns[fnKey], data, bindText, e);
+            return void error("@2", _fns[fnKey], data, bindText, e);
         }
     }
     function createFunction(body) {
         try {
             return new Function("_", body);
         } catch (e) {
-            error("#1", body);
+            error("@1", body, e);
         }
     }
     function getDataFieldCss(data, body) {
@@ -195,8 +206,17 @@
         if (el[S_ELEMENT_PROP_BIND_INFO]) return json(el[S_ELEMENT_PROP_BIND_INFO]);
         var bindInfo = parseBindInfo(el);
         if (!bindInfo) return 0;
-        if (bindInfo[S_BIND_INFO_PROP_DATA_ID] = getDataId(data), cssFields) for (var key in bindInfo) if (key != S_BIND_INFO_PROP_DATA_ID && key != BIND_KEY_FIELD && key != BIND_KEY_CLICK) if (key == BIND_KEY_STYLE || key == BIND_KEY_CLASS) for (var kv, kvs = bindInfo[key].split(";"), i = 0; i < kvs.length; i++) {
+        if (bindInfo[S_BIND_INFO_PROP_DATA_ID] = getDataId(data), cssFields) for (var key in bindInfo) if (key != S_BIND_INFO_PROP_DATA_ID && key != BIND_KEY_FIELD && key != BIND_KEY_CLICK) if (key == BIND_KEY_STYLE) for (var kv, kvs = bindInfo[key].split(";"), i = 0; i < kvs.length; i++) {
             kv = kvs[i].split("=");
+            var fields = [];
+            getBindValue(data, kv[1], fields);
+            for (var j = 0; j < fields.length; j++) cssFields.push(fields[j]);
+        } else if (key == BIND_KEY_CLASS) for (var kv, kvs = bindInfo[key].split(";"), i = 0; i < kvs.length; i++) if (kv = kvs[i].split("="), 
+        1 == kv.length) {
+            var fields = [];
+            getBindValue(data, kv[0], fields);
+            for (var j = 0; j < fields.length; j++) cssFields.push(fields[j]);
+        } else {
             var fields = [];
             getBindValue(data, kv[1], fields);
             for (var j = 0; j < fields.length; j++) cssFields.push(fields[j]);
@@ -211,15 +231,15 @@
         var kvs, kv, rs = {}, bindText = getAttr(el, DOM_ATTR_BIND);
         if (!bindText) return null;
         var tmpTxt = bindText.replace(/,{1}\s*\w+\s*:{1}/g, function(match) {
-            return "	" + match.substring(1);
+            return "^" + match.substring(1);
         });
-        return kvs = tmpTxt.split("	"), each(kvs, function(item) {
+        return kvs = tmpTxt.split("^"), each(kvs, function(item) {
             kv = item.split(S_COLON), rs[trim(kv[0]).toLowerCase()] = trim(item.substring(item.indexOf(S_COLON) + 1));
         }), rs;
     }
     function defineElement(el, data) {
-        var cssFields = [], bindInfo = getElementBindInfo(el, data, cssFields);
-        if (each(cssFields, function(css) {
+        var cssFields = [], bindInfo = getElementBindInfo(el, data, cssFields), dataid = getDataId(data);
+        if (dataid && addClass(el, dataid), each(cssFields, function(css) {
             addClass(el, css);
         }), !bindInfo[BIND_KEY_FIELD]) {
             var field = bindInfo[BIND_KEY_VALUE] || bindInfo[BIND_KEY_INNERTEXT];
@@ -239,7 +259,7 @@
                     template.appendChild(child.cloneNode(!0));
                 });
                 var templateid = uid(UID_PREFIX_TEMPLATE);
-                document[templateid] = template, setAttr(el, S_ELEMENT_ATTR_TEMPLATE, templateid), 
+                setAttr(el, S_ELEMENT_ATTR_TEMPLATE, templateid), document[templateid] = template, 
                 removeChilds(el);
             }
         });
@@ -300,6 +320,7 @@
                 editStyle(el, keys, styles.join(S_SEMICOLON));
             }
         }, 5), putRender(BIND_KEY_CLASS, function(el, data, bindText) {
+            if (data[bindText]) return void setClass(el, data[bindText]);
             var keys = [], txts = [];
             if (parseSubBindText(bindText, keys, txts), keys.length) for (var i = 0; i < keys.length; i++) getBindValue(data, txts[i]) ? addClass(el, keys[i]) : removeClass(el, keys[i]);
         }), putRender(BIND_KEY_INNERTEXT, function(el, data, bindText) {
@@ -349,15 +370,17 @@
         }
     }
     function createFragmentByTemplate(data, el, foreach) {
+        var templateid = el.$t || (el.$t = getAttr(el, S_ELEMENT_ATTR_TEMPLATE));
+        el.removeAttribute(S_ELEMENT_ATTR_TEMPLATE);
         var rs;
         return foreach ? (rs = document.createDocumentFragment(), isArray(data) && data.length > 0 && each(data, function(dt) {
             if (dt) {
                 for (var tbody, fragment = createFragmentByTemplate(dt, el), i = 0; i < fragment.childNodes.length; i++) fragment.childNodes[i].tagName == S_TBODY && (tbody = fragment.childNodes[i]);
                 if (tbody) for (var i = 0; i < tbody.childNodes.length; i++) rs.appendChild(tbody.childNodes[i]); else rs.appendChild(fragment);
             }
-        })) : (rs = document[getAttr(el, S_ELEMENT_ATTR_TEMPLATE)].cloneNode(!0), each(rs.querySelectorAll(SELECTOR_DATA_BIND), function(node) {
+        })) : (rs = document[templateid].cloneNode(!0), each(rs.querySelectorAll(SELECTOR_DATA_BIND), function(node) {
             defineElement(node, data), elementRender(node);
-        })), rs;
+        })), removeDataBindAttr(rs), rs;
     }
     function elementRender(el, bindInfo, dataField) {
         if (bindInfo = bindInfo || getElementBindInfo(el), !_renders) {
@@ -369,20 +392,24 @@
         }
         each(_renders, function(render) {
             function functionOfBind() {
+                var data = getData(bindInfo[S_BIND_INFO_PROP_DATA_ID]);
                 try {
                     var fn = getBindValue(data, bindText, 0, 1);
-                    fn.call(data, data);
+                    fn.call(this, data);
                 } catch (e) {
-                    error("#3", el, bindText, data, e);
+                    error("@3", el, bindText, data, e);
                 }
             }
-            var bindText = bindInfo[render[S_RENDER_KEY]], data = getData(bindInfo[S_BIND_INFO_PROP_DATA_ID]);
+            var bindText = bindInfo[render[S_RENDER_KEY]];
             if (bindText) {
                 if (dataField && bindText.indexOf(dataField) < 0) return;
-                render[S_RENDER_EVENT] ? addEvent(render[S_RENDER_EVENT], functionOfBind, el) : render[S_RENDER_FN](el, data, bindText);
+                if (render[S_RENDER_EVENT]) addEvent(render[S_RENDER_EVENT], functionOfBind, el); else {
+                    var data = getData(bindInfo[S_BIND_INFO_PROP_DATA_ID]);
+                    render[S_RENDER_FN](el, data, bindText);
+                }
             }
         });
-        for (var bindKey in bindInfo) if (!_renderMap[bindKey] && bindKey != S_BIND_INFO_PROP_DATA_ID) {
+        for (var bindKey in bindInfo) if (!_renderMap[bindKey] && bindKey != S_BIND_INFO_PROP_DATA_ID && bindKey != S_BIND_INFO_PROP_TEMPLATE_ID) {
             var bindText = bindInfo[bindKey];
             if (!(dataField && bindText.indexOf(dataField) < 0)) {
                 var data = getData(bindInfo[S_BIND_INFO_PROP_DATA_ID]), value = getBindValue(data, bindText);
@@ -404,11 +431,19 @@
             }
         }
     }
+    function removeDataBindAttr(dom) {
+        if (IS_REMOVE_ATTR_BIND) {
+            var els = (dom || document).querySelectorAll(SELECTOR_DATA_BIND);
+            each(els, function(el) {
+                el.removeAttribute(DOM_ATTR_BIND);
+            });
+        }
+    }
     function addEvent(ev, fun, el) {
         el.addEventListener ? el.addEventListener(ev, fun, !1) : el.attachEvent ? el.attachEvent("on" + ev, fun) : el["on" + ev] = fun;
     }
     function addClass(el, name) {
-        if ($isIE) if (el.className) {
+        if (name) if ($isIE) if (el.className) {
             var ary = el.className.split(" ");
             if (ary.indexOf(name) >= 0) return;
             ary.push(name), el.className = ary.join(" ");
@@ -419,6 +454,14 @@
             var ary = el.className.split(" "), idx = ary.indexOf(name);
             idx >= 0 && (ary.slice(idx, 1), el.className = ary.join(" "));
         } else el.classList.remove(name);
+    }
+    function setClass(el, cls) {
+        var addNames = cls.split(" ");
+        if ($isIE) el.className = ""; else {
+            for (var delNames = [], i = 0; i < el.classList.length; i++) startsWith(el.classList[i], UID_PREFIX_DATA) || delNames.push(el.classList[i]);
+            for (var i = 0; i < delNames.length; i++) el.classList.remove(delNames[i]);
+        }
+        for (var i = 0; i < addNames.length; i++) addClass(el, addNames[i]);
     }
     function setAttr(el, name, value) {
         return el.setAttribute(name, value);
@@ -435,8 +478,15 @@
     }
     function on(eventNames, fn) {
         eventNames && isFunction(fn) && eventNames.replace(REG_EVENT, function(name, pos) {
-            (_callbacks[name] = _callbacks[name] || []).push(fn);
+            for (var isExist = !1, fns = _callbacks[name] = _callbacks[name] || [], i = 0; i < fns.length; i++) if (fns[i] == fn) {
+                isExist = !0;
+                break;
+            }
+            !isExist && fns.push(fn);
         });
+    }
+    function once(eventNames, fn) {
+        fn.once = 1, on(eventNames, fn);
     }
     function off(eventNames, fn) {
         eventNames && "*" != eventNames ? fn ? eventNames.replace(REG_EVENT, function(name) {
@@ -448,7 +498,10 @@
     function trigger(eventNames, eventObj) {
         var args = slice.call(arguments, 1);
         eventNames.replace(REG_EVENT, function(name) {
-            for (var fns = _callbacks[name] || [], i = 0; i < fns.length; i++) tryApply(eventObj, fns[i], args);
+            for (var fns = _callbacks[name] || [], i = 0; i < fns.length; i++) {
+                var fn = fns[i];
+                fn.once && off(name, fn), tryApply(eventObj, fn, args);
+            }
         });
     }
     function notify(delay, eventNames, eventObj) {
@@ -461,7 +514,7 @@
         opt && settings(opt), installRenders(), observe(data);
         var dom = selector ? document.querySelector(selector) : document;
         parseHtml(data, dom), showHtml(dom), on(EVENT_UPDATE_VIEW, updateviewEventListener), 
-        addEvent("change", datachangeEventListener, document);
+        addEvent("change", datachangeEventListener, document), removeDataBindAttr(dom);
     }
     var S_LEFT_ZKH = "[", S_XING_EQ = "*=", S_RIGHT_ZKH = "]", S_OBJECT_OBJECT = "[object Object]", S_FUNCTION = "function", S_OBJECT = "object", S_VISIBILITY = "visibility", S_DISPLAY = "display", S_COMMA = ",", S_COLON = ":", S_DOT = ".", S_SEMICOLON = ";", S_EQUAL = "=", S_BLANK = "", S_STYLE = "style", S_VISIBLE = "visible", S_NONE = "none", S_TBODY = "TBODY", S_STAR = "*", S_MINUS = "-", $isIE = window == document && document != window;
     "number" == typeof window.screenX;
@@ -469,9 +522,9 @@
         for (var i = start || 0; i < this.length; i++) if (this[i] === item) return i;
         return -1;
     });
-    var DEBUG, DOM_ATTR_BIND, SELECTOR_DATA_BIND, DATA_KEY_FN_ID, DATA_KEY_FN_PARENT, DATA_KEY_FN_ROOT, DATA_KEY_FN_DATA, BIND_REF_ROOT, BIND_REF_PARENT, BIND_REF_DATA, BIND_KEY_FIELD, BIND_KEY_VALUE, BIND_KEY_INNERTEXT, BIND_KEY_INNERHTML, BIND_KEY_OPTIONS, BIND_KEY_READONLY, BIND_KEY_DISABLED, BIND_KEY_VISIBLE, BIND_KEY_CHECKED, BIND_KEY_STYLE, BIND_KEY_CLASS, BIND_KEY_FOREACH, BIND_KEY_WITH, BIND_KEY_IF, BIND_KEY_CLICK, EVENT_DATA_CHAGE, EVENT_UPDATE_VIEW, UID_PREFIX_DATA, UID_PREFIX_TEMPLATE, Options = {}, S_BIND_INFO_PROP_DATA_ID = "$d";
+    var DEBUG, IS_REMOVE_ATTR_BIND, DOM_ATTR_BIND, SELECTOR_DATA_BIND, DATA_KEY_FN_ID, DATA_KEY_FN_PARENT, DATA_KEY_FN_ROOT, DATA_KEY_FN_DATA, BIND_REF_ROOT, BIND_REF_PARENT, BIND_REF_DATA, BIND_KEY_FIELD, BIND_KEY_VALUE, BIND_KEY_INNERTEXT, BIND_KEY_INNERHTML, BIND_KEY_OPTIONS, BIND_KEY_READONLY, BIND_KEY_DISABLED, BIND_KEY_VISIBLE, BIND_KEY_CHECKED, BIND_KEY_STYLE, BIND_KEY_CLASS, BIND_KEY_FOREACH, BIND_KEY_WITH, BIND_KEY_IF, BIND_KEY_CLICK, EVENT_DATA_CHAGE, EVENT_UPDATE_VIEW, UID_PREFIX_DATA, UID_PREFIX_TEMPLATE, Options = {}, S_BIND_INFO_PROP_DATA_ID = "$d", S_BIND_INFO_PROP_TEMPLATE_ID = "$t";
     initOptionValues();
-    var _renderMap, _renders, slice = Array.prototype.slice, async = setTimeout, _mapDataIdDataObject = {}, S_REG_QUOTE = /(['"])[^'"]*\1/g, S_REG_PROP = /([\w$_\.\[\]]+)/g, _fns = {}, S_ELEMENT_PROP_BIND_INFO = "_bindInfo", S_ELEMENT_ATTR_TEMPLATE = "_template", S_IGNORE_KEY = "true false null alert this _ if else".split(" "), S_RENDER_KEY = "k", S_RENDER_FN = "f", S_RENDER_ORDER = "o", S_RENDER_TEMPLATE = "t", S_RENDER_EVENT = "e", REG_EVENT = /\S+/g, _callbacks = {};
+    var _renderMap, _renders, slice = Array.prototype.slice, async = setTimeout, _mapDataIdDataObject = {}, S_REG_QUOTE = /(['"])[^'"]*\1/g, S_REG_PROP = /([\w$_\.\[\]]+)/g, _fns = {}, S_ELEMENT_PROP_BIND_INFO = "_bindInfo", S_ELEMENT_ATTR_TEMPLATE = "_template", EVENT_CHECK_MEMORY = "_eventCheckMap", S_IGNORE_KEY = "true false null alert this _ if else".split(" "), S_RENDER_KEY = "k", S_RENDER_FN = "f", S_RENDER_ORDER = "o", S_RENDER_TEMPLATE = "t", S_RENDER_EVENT = "e", REG_EVENT = /\S+/g, _callbacks = {};
     window.addEventListener("beforeunload", function(event) {
         _mapDataIdDataObject = null, _renderMap = null, _renders = null, _callbacks = null, 
         _fns = null;
